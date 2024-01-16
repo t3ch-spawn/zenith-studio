55b4690af2d10779cc277d77c9f8d9b4fa5bceb7
Tife <boluwatife1904@gmail.com>
2024-01-16 14:34:19 +0100

    asjusted  work together section

--- src/components/TogetherMarque.jsx
+++ src/components/TogetherMarque.jsx
@@ -7,20 +7,20 @@ export default function TogetherMarque() {
     gsap.registerPlugin(ScrollTrigger);
 
     ScrollTrigger.create({
-      trigger: ".categories-cont",
+      trigger: ".subscribe-container",
       scrub: 1,
-      start: "bottom 50%",
+      start: "top 60%",
       endTrigger: ".marque-container",
-      //   end: "bottom 50%",
+        // end: "bottom 20%",
         // markers: true,
       animation: gsap.to(".marque", {
-        x: "-60%",
+        x: "-50%",
       }),
     });
   }, []);
   return (
     <div className="w-full overflow-hidden relative h-[100px] marque-container py-10">
-      <div className="marque w-[100%] text-6xl -950:text-4xl -500:text-2xl absolute translate-x-[-20%] -500:translate-x-[-50px] h-full text-nowrap">
+      <div className="marque w-[100%] text-6xl -950:text-4xl -500:text-2xl absolute  h-full text-nowrap">
         LET’S WORK TOGETHER? - GET IN TOUCH - LET’S WORK TOGETHER? - GET IN TOUCH
       </div>
     </div>
