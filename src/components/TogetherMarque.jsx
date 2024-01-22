51d96e5226a39238aea4d0c048c27a7ddb93b56a
Tife <boluwatife1904@gmail.com>
2024-01-22 18:05:13 +0100

    adjusted marquee

--- src/components/TogetherMarque.jsx
+++ src/components/TogetherMarque.jsx
@@ -22,7 +22,7 @@ export default function TogetherMarque() {
     <div className="w-full marque-container overflow-hidden relative h-[100px] marque-container z-[15] text-[#353434] bg-[#F3EEEE] pb-[150px] -950:pb-[120px] -550:pb-[110px] font-medium">
       <p className="marque w-[100%] text-6xl -950:text-4xl -500:text-2xl absolute py-10 h-full text-nowrap">
         LET’S WORK TOGETHER? - GET IN TOUCH - LET’S WORK TOGETHER? - GET IN
-        TOUCH
+        TOUCH - LET’S WORK TOGETHER? - GET IN TOUCH
       </p>
 
       <div className="absolute w-full h-[1px] bg-[#A39E9F] bottom-0 mt-10"></div>
